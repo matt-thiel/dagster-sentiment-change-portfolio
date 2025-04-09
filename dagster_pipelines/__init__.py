b344b487473c1c5a3d61b383a6c4db93e7bd0ec7
greg-vbase <greg@vbase.com>
2025-04-08 18:16:17 -0700

    Fixed build

---
+++ dagster_pipelines/__init__.py
@@ -0,0 +1,5 @@
+# Dagster Cloud is importing your package like: import dagster_pipelines
+# and expects to find dagster_pipelines.defs
+# This re-exports your defs object from definitions.py at the package level,
+# which Dagster Cloud requires.
+from .definitions import defs
